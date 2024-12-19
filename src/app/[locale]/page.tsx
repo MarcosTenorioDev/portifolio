9f07da8d15a62d690a669fa880863707048f569d
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-12-19 14:44:26 -0300

    Feat: add experience section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,17 +15,18 @@ import ProfileCard from "@/components/ProfileCard";
 import TechSkillsCard from "@/components/TechSkillsCard";
 import DetailsProfileCard from "@/components/DetailsProfileCard";
 import DescriptionProfileCard from "@/components/DescriptionProfileCard";
+import ExperienceCard from "@/components/ExperienceCardProps";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
-    <div className="grid font-roboto grid-rows-[20px_1fr_20px] max-w-[1680px] mx-auto items-start justify-items-center min-h-screen gap-20 px-10 md:px-20 pt-6">
+    <div className="grid pb-10 font-roboto max-w-[1680px] mx-auto items-start justify-items-center gap-20 px-10 md:px-20 pt-6">
       <BigNavbar />
       <div className="w-full mt-6 h-full flex flex-col xl:flex-row gap-10">
         <div className="w-full">
           <ProfileCard />
         </div>
-        <div className="flex flex-col h-full gap-12 max-h-[700px]">
+        <div className="flex flex-col h-full gap-12">
           <div className="h-fit flex flex-col sm:flex-row gap-10">
             <DescriptionProfileCard />
             <DetailsProfileCard />
@@ -34,6 +35,15 @@ export default function HomePage() {
             <TechSkillsCard />
           </div>
         </div>
+      </div>
+      <div className="w-full">
+        <h2 className="text-center lg:text-end w-full text-[72px] italic font-bold mb-6">2 YEARS OF <br/>EXPERIENCE</h2>
+        <ExperienceCard
+        title="Direction Systems"
+        subtitle="Desenvolvedor Fullstack"
+        description="Atuei como desenvolvedor fullstack, com experiência em Front-end utilizando Angular, React e Bootstrap, e no Back-end com .NET C#. Durante minha trajetoria, desenvolvi integrações de APIs, implementei gateways de pagamento e contribui ativamente no gerenciamento de projetos. Além disso, trabalhei com metodologias ageis, promovendo uma colaboração eficiente em equipe."
+        dateRange="Nov-2023 / Jul-2024"
+      />
       </div>
     </div>
   );
---
+++ src/components/ExperienceCardProps.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useState } from 'react'
+import { Card } from '@/components/ui/card'
+
+interface ExperienceCardProps {
+  title: string
+  subtitle: string
+  description: string
+  dateRange: string
+  textColor?: string
+}
+
+export default function ExperienceCard({
+  title,
+  subtitle,
+  description,
+  dateRange,
+}: ExperienceCardProps) {
+  const [isHovered, setIsHovered] = useState(false)
+
+  return (
+    <Card
+      className="relative w-full overflow-hidden transition-all duration-300 border-none font-roboto bg-transparent text-justify shadow-none hover:bg-muted rounded-[4px]"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="p-6">
+        <div className="space-y-4">
+          <div>
+            <h2 className="text-2xl italic font-bold">{title}</h2>
+            <p className="text-opacity-70 italic font-light">{subtitle}</p>
+          </div>
+          
+          <p className="text-sm leading-relaxed">{description}</p>
+
+          <div className="text-sm font-extrabold">
+            {dateRange}
+          </div>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
