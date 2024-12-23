a76e607e4b8388775b49739bf2c395e47263062e
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-12-23 19:59:10 -0300

    Feat: add not found page

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import { Roboto_Mono, Space_Grotesk } from "next/font/google";
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  style: ["normal", "italic"],
+});
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-grotesk",
+  subsets: ["latin"],
+  weight: ["700"],
+  style: ["normal"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${robotoMono.variable} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,45 @@
-import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import notFoundImage from "@/../public/images/404.avif";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 
 const NotFound = () => {
   return (
-    <html>
-      <body className="text-center">
-        <div className="flex min-h-screen flex-col items-center justify-center text-center p-6">
-          <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-          <p className="text-lg mb-8">
-            Oops! The page you are looking for does not exist.
-          </p>
-          <Link
-            href="/"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
-          >
-            Go Back Home
-          </Link>
+    <div className="bg-black font-roboto">
+      <Image
+        src={notFoundImage}
+        alt="404 - Page Not Found"
+        fill={true}
+        objectFit="cover"
+        quality={100}
+        priority
+      />
+      <div className="absolute inset-0 flex flex-col justify-between border-[25px] border-black overflow-hidden">
+        <Link
+          href="/"
+          passHref
+          aria-label="Voltar para a página inicial"
+          title="Página inicial"
+          className={cn(
+            buttonVariants({ variant: "link", size: "lg" }),
+            "bg-black text-white clip-custom-button py-6 w-min !border-0"
+          )}
+        >
+          <ArrowLeft /> Back to homepage
+        </Link>
+        <div
+          className={cn(
+            buttonVariants({ variant: "default", size: "lg" }),
+            "bg-black text-white clip-custom-button self-end py-6 w-min !border-0"
+          )}
+        >
+          404 - Page Not Found
         </div>
-      </body>
-    </html>
+      </div>
+    </div>
   );
 };
 
