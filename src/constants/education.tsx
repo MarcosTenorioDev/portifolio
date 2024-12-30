cf2d6e434189f0a84fc2f6d0e8d80b27c4160184
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-12-30 11:55:29 -0300

    Fix education texts and image

--- src/constants/education.tsx
+++ src/constants/education.tsx
@@ -8,7 +8,7 @@ export const education = [
       alt: "Logo da Universidade Católica de Pernambuco",
       hoverImage: "/images/academic/banner-unicap.webp",
       description:
-        "O curso visa formar profissionais capazes de desenvolver, implementar e gerenciar sistemas web, utilizando as tecnologias e metodologias mais modernas da área.",
+        "Formação superior em Tecnologia em Sistemas para Internet pela Universidade Católica de Pernambuco, com bolsa integral e coeficiente de 9,4, sou capacitado a desenvolver soluções completas em Front-end, Back-end e Mobile, sempre com foco em inovação, excelência e qualidade.",
     },
     en: {
       logo: "/images/academic/unicap-logo.webp",
@@ -18,7 +18,7 @@ export const education = [
       alt: "Catholic University of Pernambuco logo",
       hoverImage: "/images/academic/banner-unicap.webp",
       description:
-        "The course aims to train professionals capable of developing, implementing and managing web systems, using the most modern technologies and methodologies in the area.",
+        "Higher Education in Internet Systems Technology from the Catholic University of Pernambuco with a full scholarship and a GPA of 9.4 (on a 10-point scale). I am proficient in delivering complete solutions in frontend, backend, and mobile development, always focusing on innovation, excellence and quality.",
     },
   },
   {
@@ -28,9 +28,9 @@ export const education = [
       institution: "Ferreira Costa - FCx Labs",
       dateRange: "Fev-2023 / Jul-2023 (5 Meses)",
       alt: "Logo Ferreira Costa - FCx Labs",
-      hoverImage: "/images/academic/banner-unicap.webp",
+      hoverImage: "/images/academic/banner-fcx.webp",
       description:
-        "A residência visa formar profissionais capazes de desenvolver, implementar e gerenciar sistemas web, utilizando as tecnologias e metodologias mais modernas da área.",
+        "Atuei como líder no desenvolvimento frontend e desenvolvedor backend na criação de um Sistema de Gerenciamento de Pedidos (Order Management System - OMS) na Ferreira Costa, em parceria com o Porto Digital, utilizando tecnologias como ReactJS, Chakra UI, NodeJS, Postgres e TypeScript.",
     },
     en: {
       logo: "/images/academic/fcx-logo.webp",
@@ -38,9 +38,9 @@ export const education = [
       institution: "Ferreira Costa - FCx Labs",
       dateRange: "Feb-2023 / Jul-2023 (5 Months)",
       alt: "Ferreira Costa - FCx Labs logo",
-      hoverImage: "/images/academic/banner-unicap.webp",
+      hoverImage: "/images/academic/banner-fcx.webp",
       description:
-        "The residency aims to train professionals capable of developing, implementing and managing web systems, using the most modern technologies and methodologies in the area.",
+        "I worked as a frontend lead and backend developer in the creation of an Order Management System (OMS) at Ferreira Costa, in partnership with Porto Digital, using technologies such as ReactJS, Chakra UI, NodeJS, Postgres, and TypeScript.",
     },
   },
 ];
--- src/constants/experience.tsx
+++ src/constants/experience.tsx
@@ -5,14 +5,14 @@ export const experiences = [
       subtitle: "Desenvolvedor Fullstack",
       description:
         "Atuei como desenvolvedor fullstack, com experiência em Front-end utilizando Angular, React e Bootstrap, e no Back-end com .NET C#. Durante minha trajetoria, desenvolvi integrações de APIs, implementei gateways de pagamento e contribui ativamente no gerenciamento de projetos. Além disso, trabalhei com metodologias ageis, promovendo uma colaboração eficiente em equipe.",
-      dateRange: "Nov-2023 / Dez-2024",
+      dateRange: "Nov-2023 / Jul-2024",
     },
     en: {
       title: "Direction Systems",
       subtitle: "Fullstack Developer",
       description:
         "I worked as a fullstack developer, with experience in Front-end using Angular, React and Bootstrap, and in the Back-end with .NET C#. During my journey, I developed API integrations, implemented payment gateways and actively contributed to project management. In addition, I worked with agile methodologies, promoting efficient collaboration in teams.",
-      dateRange: "Nov-2023 / Dec-2024",
+      dateRange: "Nov-2023 / Jul-2024", 
     },
   },
 ];
