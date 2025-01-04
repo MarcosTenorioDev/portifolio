9c15bf5818a83fd889898dc7e7cbea2266afa86d
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2025-01-04 08:56:44 -0300

    Feat: add cv to download

--- src/components/DescriptionProfileCard.tsx
+++ src/components/DescriptionProfileCard.tsx
@@ -2,9 +2,14 @@ import { FileDown } from "lucide-react";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import ButtonAnimated from "./ButtonAnimated";
 import { useTranslations } from "next-intl";
+import { useParams } from "next/navigation";
 
 const DescriptionProfileCard = () => {
   const t = useTranslations("HomePage");
+  const params = useParams()
+  const locale = params.locale
+  const filePath = `/cv-${locale}.pdf`;
+
   return (
     <div className="card-wrap">
       <Card className="clip-custom-card w-full pt-6 flex flex-col justify-between">
@@ -15,7 +20,7 @@ const DescriptionProfileCard = () => {
         </CardContent>
         <CardFooter>
           <div className="max-w-[250px] w-full mx-auto">
-            <ButtonAnimated className="p-6 px-8">
+            <ButtonAnimated className="p-6 px-8"  onClick={() => window.open(filePath)}>
               {t("DescriptionCard.ButtonLabel")} <FileDown />
             </ButtonAnimated>
           </div>
